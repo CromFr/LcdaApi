e536c773941e7bd673fc93247a397900ebf5e63b
Crom (Thibaut CHARLES) <CromFr@gmail.com>
2016-03-23 02:43:36 +0100

    Big work...

--- source/app.d
+++ source/app.d
@@ -1,15 +1,28 @@
 import vibe.d;
 import std.stdio;
+import nwn2.resman;
+import nwn2.tlk;
+
 
 
 int main(string[] args){
+
+	//TODO: get from config
+	ResMan.path ~= "/home/crom/Documents/Neverwinter Nights 2/override/LcdaClientSrc/lcda2da.hak/";
+	auto strresolv = new StrRefResolver(
+		new Tlk("/home/crom/.wine-nwn2/drive_c/Neverwinter Nights 2/dialog.TLK"),
+		new Tlk("/home/crom/Documents/Neverwinter Nights 2/tlk/Lcda.tlk"));
+	ResMan.addRes("resolver", strresolv);
+
+
 	auto settings = new HTTPServerSettings;
 	settings.port = 8080;
 	//settings.sessionStore = new MemorySessionStore;
 	settings.sessionStore = new RedisSessionStore("127.0.0.1", 1);
 
 	auto router = new URLRouter;
-	router.get("*", serveStaticFiles("public/"));
+	router.get("*", serveStaticFiles("public/"));//TODO: not great
+	//router.get("/nwn2", serveStaticFiles("/home/crom/.wine-nwn2/drive_c/Neverwinter Nights 2/UI/default/images"));//TODO: not great
 	router.registerWebInterface(new WebInterface);
 
 	listenHTTP(settings, router);
@@ -36,6 +49,7 @@ class WebInterface {
 		import nwn2.character;
 		import std.file;
 		import std.path;
+		import std.algorithm;
 
 		auto vault = DirEntry(buildNormalizedPath(
 			"/home/crom/Documents/Neverwinter Nights 2/servervault/",//TODO: get from config
@@ -45,18 +59,32 @@ class WebInterface {
 		foreach(d ; vault.dirEntries("*.bic", SpanMode.depth)){
 			charList ~= new Character(d);
 		}
+		charList.sort!"a.name<b.name";
+
+		Character[] supprCharList;
 
 		enum page = "charlist";
 		auto session = this;
-		render!("page.dt", page, session, charList);
+		render!("page.dt", page, session, charList, supprCharList);
 	}
 
 	@path("/:accountToView/:char")
 	void getChar(HTTPServerRequest req, HTTPServerResponse res){
+		enforceHTTP(authenticated, HTTPStatus.forbidden);
+		enforceHTTP(admin || account==req.params["accountToView"], HTTPStatus.forbidden);
+
+		import nwn2.character;
+		import std.path;
+
+		auto character = new Character(buildNormalizedPath(
+			"/home/crom/Documents/Neverwinter Nights 2/servervault/",//TODO: get from config
+			req.params["accountToView"],
+			req.params["char"]~".bic"
+			));
 
 		enum page = "char";
 		auto session = this;
-		render!("page.dt", page, session);
+		render!("page.dt", page, session, character);
 	}
 
 	void postLogin(string login, string password){
--- source/nwn2/character.d
+++ source/nwn2/character.d
@@ -1,26 +1,94 @@
 module nwn2.character;
-import nwn2.gff;
+
+import std.conv;
 
 class Character{
 
 	this(in string bicFile){
+		import std.path : baseName;
+		import nwn2.resman;
 		import nwn2.gff;
-		import std.stdio;
+		import nwn2.tlk;
+		import nwn2.twoda;
 
+		this.bicFile = bicFile;
+		bicFileName = baseName(bicFile, ".bic");
+
+		auto strref = ResMan.get!StrRefResolver("resolver");
 		auto gff = new Gff(bicFile);
+		auto class2da = ResMan.findFileRes!TwoDA("classes.2da");
+		auto race2da = ResMan.findFileRes!TwoDA("racialsubtypes.2da");
+		auto abilities2da = ResMan.findFileRes!TwoDA("iprp_abilities.2da");
+		auto alignment2da = ResMan.findFileRes!TwoDA("iprp_alignment.2da");
 
-
+		//Name
 		name = gff["FirstName"].to!string~" "~gff["LastName"].to!string;
 
+		//Level / classes
 		lvl = 0;
 		foreach(n ; gff["ClassList"].to!(GffNode[])){
-			lvl += n["ClassLevel"].to!int;
+			immutable classID = n["Class"].to!int;
+			immutable classLvl = n["ClassLevel"].to!int;
+
+			lvl += classLvl;
+			classes[strref.get(class2da.get!uint("Name", classID))] = classLvl;
+		}
+
+		//Race
+		race = strref.get(race2da.get!uint("Name", gff["Subrace"].to!int));
+
+		//Alignment
+		alignment.good_evil = gff["GoodEvil"].to!int;
+		alignment.law_chaos = gff["LawfulChaotic"].to!int;
+		alignment.id = 0;
+		alignment.id += alignment.good_evil>=75? 0 : alignment.good_evil>25? 1 : 2;
+		alignment.id += alignment.law_chaos>=75? 0 : alignment.law_chaos>25? 3 : 6;
+		alignment.name = strref.get(alignment2da.get!uint("Name", alignment.id));
+
+		//God
+		god = gff["Deity"].to!string;
+
+		//Abilities
+		foreach(i ; 0..6){
+			abilities ~= Ability(
+				strref.get(abilities2da.get!uint("Name", i)),
+				gff[abilities2da.get!string("Label", i)].to!int
+			);
 		}
 	}
 
 
 	string name;
-	int lvl;
 
+	int lvl;
+	int[string] classes;
+
+	string race;
+
+	struct Alignment{
+		uint id;
+		string name;
+		int good_evil;
+		int law_chaos;
+	}
+	Alignment alignment;
+	string god;
+
+	struct Ability{string label; int value;}
+	Ability[] abilities;
+
+
+
+	immutable string bicFile;
+	immutable string bicFileName;
+
+
+	string classesToString(){
+		string ret;
+		foreach(name, lvl ; classes){
+			ret ~= name~" ("~lvl.to!string~") ";
+		}
+		return ret;
+	}
 
 }
