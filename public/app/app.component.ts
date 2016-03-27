07f517fe35a9c8c515c542a3b8331dc1383c47fd
Crom (Thibaut CHARLES) <CromFr@gmail.com>
2016-03-27 18:45:26 +0200

    Almost working materialize (exception on second #modal-login close)

--- public/app/app.component.ts
+++ public/app/app.component.ts
@@ -1,13 +1,27 @@
-import {Component} from "angular2/core";
+import {Component, AfterViewInit} from "angular2/core";
 
 import {CharListComponent} from "./charlist.component";
+
+declare var $: any;
 
 @Component({
     selector:    "app",
     directives:  [CharListComponent],
     templateUrl: "app/app.component.html",
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
     public isLoggedIn: boolean = false;
     public account: string;
+
+
+
+    ngAfterViewInit() {
+        if (!this.materializeInit) {
+            this.materializeInit = true;
+            $("#modal-login-button").leanModal();
+            $("#sidebar-button").sideNav();
+        }
+    }
+
+    private materializeInit: boolean = false;
 }
