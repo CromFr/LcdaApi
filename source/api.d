9771c2ca430e247bba6e4309002f1c216c507650
Crom (Thibaut CHARLES) <CromFr@gmail.com>
2016-03-28 14:42:45 +0200

    server: delete character

--- source/api.d
+++ source/api.d
@@ -61,7 +61,16 @@ private string implementJsonIface(API)(){
 	}
 	return ret;
 }
-
+/// Json API
+///    /api/login login password
+///    /api/logout
+///    /api/:account/characters/list
+///    /api/:account/characters/:char/
+///    /api/:account/characters/:char/delete
+///  - /api/:account/characters/:char/undelete
+///    /api/:account/characters/deleted/:char/
+///  - /api/:account/characters/deleted/:char/undelete
+///
 @path("/api")
 class Api{
 
@@ -71,7 +80,7 @@ class Api{
 package:
 	alias CharList = Tuple!(Character[],"active",Character[],"deleted");
 
-	@path("/:account/char/list")
+	@path("/:account/characters/list")
 	CharList _getCharList(string _account){
 		//enforceHTTP(req.session, HTTPStatus.unauthorized);
 		//enforceHTTP(authenticated, HTTPStatus.unauthorized);
@@ -95,8 +104,6 @@ package:
 				.sort!"a.name<b.name"
 				.array;
 
-
-
 		auto deletedVaultPath = buildNormalizedPath(activeVault, "deleted");
 		Character[] deletedChars = null;
 		if(deletedVaultPath.exists && deletedVaultPath.isDir){
@@ -110,22 +117,54 @@ package:
 
 		return CharList(activeChars, deletedChars);
 	}
-	@path("/:account/char/:char")
-	Character _getCharInfo(string _account, string _char){
+	@path("/:account/characters/:char")
+	Character _getActiveCharInfo(string _account, string _char){
 		//enforceHTTP(authenticated, HTTPStatus.forbidden);
 		//enforceHTTP(admin || _account==account, HTTPStatus.forbidden);
-
-		import std.file : DirEntry;
+		return getCharInfo(_account, _char);
+	}
+	@path("/:account/characters/deleted/:char")
+	Character _getDeletedCharInfo(string _account, string _char){
+		//enforceHTTP(authenticated, HTTPStatus.forbidden);
+		//enforceHTTP(admin || _account==account, HTTPStatus.forbidden);
+		return getCharInfo(_account, _char, true);
+	}
+	@path("/:account/characters/:char/delete")
+	void _postDeleteChar(string _account, string _char){
+		import std.file : exists, isDir, rename, mkdir;
 		import std.path : buildNormalizedPath;
 
-		return new Character(DirEntry(buildNormalizedPath(
-				"/home/crom/Documents/Neverwinter Nights 2/servervault/",//TODO: get from config
-				_account,
-				_char~".bic"
-				)));
+		auto accountVault = buildNormalizedPath(
+			"/home/crom/Documents/Neverwinter Nights 2/servervault/",
+			_account);
+
+		auto deletedFolder = buildNormalizedPath(accountVault, "deleted");
+
+		if(!deletedFolder.exists){
+			mkdir(deletedFolder);
+		}
+
+
+		auto target = buildNormalizedPath(deletedFolder, _char~".bic");
+		if(target.exists){
+			import std.regex : ctRegex, matchFirst;
+			enum rgx = ctRegex!r"^(.+?)(\d*)$";
+			auto match = _char.matchFirst(rgx);
+			if(match.empty)throw new Exception("Unable to parse character name: "~_char);
+
+			string newName = match[1];
+			int index = match[2].to!int;
+
+			while(target.exists){
+				target = buildNormalizedPath(deletedFolder, newName~(++index).to!string~".bic");
+			}
+		}
+
+		import std.stdio : writeln;
+		writeln("Renaming '",accountVault,"' to '",target,"'");
+		rename(accountVault, target);
 	}
-	@path("/:account/char/:char/delete")
-	void _postDeleteChar(string _account, string _char){}
+
 	void _postLogin(string login, string password){
 		import mysql;
 		import nwn2.resman;
@@ -153,5 +192,19 @@ private:
 	SessionVar!(bool, "authenticated") authenticated;
 	SessionVar!(bool, "admin") admin;
 	SessionVar!(string, "account") account;
+
+
+	Character getCharInfo(in string account, in string bicName, bool deleted=false){
+		import std.file : DirEntry;
+		import std.path : buildNormalizedPath;
+
+		return new Character(DirEntry(buildNormalizedPath(
+			"/home/crom/Documents/Neverwinter Nights 2/servervault/",//TODO: get from config
+			account,
+			deleted? "deleted" : "",
+			bicName~".bic"
+			)));
+	}
+
 
 }
